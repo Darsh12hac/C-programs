1223826b0931c26d865c8748223b28f2551d2467
Darsh Vishwakarma <73699726+Darsh12hac@users.noreply.github.com>
2024-02-22 22:08:42 +0530

    Update Print1to10.c

--- Print1to10.c
+++ Print1to10.c
@@ -6,8 +6,8 @@ int main(){
    for(int i=1;i<11;i++){
     sum+=i;
     printf("%d - " , i);
-    printf(" Sum = %d\n",sum);
+  //  printf(" Sum = %d\n",sum);
    }
-  // printf(" Sum = %d\n",sum);
+   printf(" Sum = %d\n",sum);
 
 }
